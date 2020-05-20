34b47aa70073dea4c427c0c91cec3080e4b5a0ac
Kevin Moore <kevmoo@google.com>
2020-05-20 14:55:13 -0700

    Migrate to null safety

--- lib/src/digest_sink.dart
+++ lib/src/digest_sink.dart
@@ -7,24 +7,18 @@ import 'digest.dart';
 /// A sink used to get a digest value out of `Hash.startChunkedConversion`.
 class DigestSink extends Sink<Digest> {
   /// The value added to the sink, if any.
-  Digest get value {
-    assert(_value != null);
-    return _value;
-  }
+  Digest get value => _value;
 
-  Digest _value;
+  late Digest _value;
 
   /// Adds [value] to the sink.
   ///
   /// Unlike most sinks, this may only be called once.
   @override
   void add(Digest value) {
-    assert(_value == null);
     _value = value;
   }
 
   @override
-  void close() {
-    assert(_value != null);
-  }
+  void close() {}
 }
