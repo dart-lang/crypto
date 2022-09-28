16c6475f753c60f1f50a402aa226399919c1cfda
Michael Thomsen <mit@google.com>
2022-09-28 09:10:37 +0200

    CI test

--- lib/crypto.dart
+++ lib/crypto.dart
@@ -3,7 +3,7 @@
 // BSD-style license that can be found in the LICENSE file.
 
 export 'src/digest.dart';
-export 'src/hash.dart';
+  export 'src/hash.dart';
 export 'src/hmac.dart';
 export 'src/md5.dart';
 export 'src/sha1.dart';
