e175a95619ce23bfbd2ab34a4066f86aa0c108db
Devon Carew <devoncarew@google.com>
2022-11-03 14:33:17 -0700

    refactor tests to use a more compact encoding (#134)

--- test/hmac_sha2_test.dart
+++ test/hmac_sha2_test.dart
@@ -2,7 +2,6 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-// Library tag to allow the test to run on Dartium.
 import 'package:crypto/crypto.dart';
 import 'package:test/test.dart';
 
